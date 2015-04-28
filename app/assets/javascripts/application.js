7372c356261852b7eb1461c2fe5b1f0086f7e02a
marvinick <marviluw@gmail.com>
2015-04-27 19:11:52 -0500

    navbar js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,20 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+   var scroll_start = 0;
+   var startchange = $('.navbar');
+   var offset = startchange.offset();
+   $(document).scroll(function() {
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $('.navbar').css('background-color', 'rgba(34,34,34,0.9)');
+       } else {
+          $('.navbar').css('background-color', 'transparent');
+       }
+   });
+});
